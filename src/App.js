814f4b475cf555ade393e5db7adbd07efe066bcb
saranga96 <sarangamadhushan1996@gmail.com>
2022-07-15 17:18:09 +0530

    deploy demo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1>Hello This is my first react app</h1>
+      
+    </div>
+  );
+}
+
+export default App;
